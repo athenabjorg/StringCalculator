9618654ce0daa406e8cfc855458bb4eb36fcc018
Aþena Björg <athenabjorg@gmail.com>
2017-10-13 17:12:14 +0000

    Adding multiple numbers works

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -12,10 +12,20 @@ public class Calculator {
 			if(text.contains(","))
 			{
 				String numbers[] = text.split(",");
-				return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+				int total = 0;
+				for(String number : numbers)
+				{
+					total += toInt(number);
+				}
+				return total;
 			}
 			return 1;
 		}
 		
+	}
+
+	private static int toInt(String number)
+	{
+		return Integer.parseInt(number);
 	}
 }
